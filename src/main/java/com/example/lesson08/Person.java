8f24e257fb60045f5d7f11c446d520e0269ed525
nosukja99 <nosukja99@gmail.com>
2018-06-25 14:56:20 -0400

    Initial commit

---
+++ src/main/java/com/example/lesson08/Person.java
@@ -0,0 +1,41 @@
+package com.example.lesson08;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long Id;
+
+    private String firstName;
+    private String lastName;
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
